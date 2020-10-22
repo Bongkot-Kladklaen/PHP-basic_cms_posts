a0de589ed1b34651c7deb0379a8b7f8fccf7952c
Jaybong <bongkot.klad@gmail.com>
2020-10-22 15:25:40 +0700

    edit database for git

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
-    define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
-    define('DB_USER', 'bc3f6420d0737e');
-    define('DB_PASS', '959612c8');
-    define('DB_NAME', 'heroku_a9941dafedbd67a');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'basiccms');
 ?>
